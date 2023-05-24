5281b6b765cb1bb8b249b96f180dcb60268de54f
Gerrit Vermeulen <gerrit@nomanini.com>
2023-05-24 09:34:56 +0200

    Add crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'firebase_options.dart';
 import 'package:coffee_journal/bloc/brew_bloc.dart';
 import 'package:coffee_journal/brew_details.dart';
@@ -14,11 +17,24 @@ import 'extensions.dart';
 
 import 'dart:developer' as developer;
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // Pass all uncaught "fatal" errors from the framework to Crashlytics
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterError(errorDetails);
+  };
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
+
   runApp(MyApp());
 }
 
